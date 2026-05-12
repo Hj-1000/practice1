a99341e4922355767171d7fd7f782e4cc5376559
HJ1000 <cjsguswhd1369@gmail.com>
2026-05-12 14:55:47 +0900

    DTO 생성 2026-05-12

---
+++ src/main/java/com/jeonhong/book/springboot/web/dto/HelloResponseDTO.java
@@ -0,0 +1,14 @@
+package com.jeonhong.book.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDTO {
+
+    private final String name;
+    private final int amount;
+
+
+}
