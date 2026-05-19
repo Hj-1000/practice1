1f341209c6296a35556d1591d7d8470d052f6440
HJ1000 <cjsguswhd1369@gmail.com>
2026-05-19 19:23:32 +0900

    게시글 수정 기능 페이지까지 구현 완료 테스트 성공 2026-05-19

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -3,6 +3,10 @@ var main = {
         var _this = this;
         $('#btn-save').on('click', function () {
             _this.save();
+        })
+
+        $('#btn-update').on('click', function () {
+                _this.update();
         });
 
     },
@@ -25,7 +29,30 @@ var main = {
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
+    },
+
+    update: function () {
+        var data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/posts/' +id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function () {
+            alert('글이 수정되었습니다.');
+            window.location.href = '/'; // 글 등록에 성공하면 메인페이지로 이동
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
     }
+
 };
 
 main.init();
