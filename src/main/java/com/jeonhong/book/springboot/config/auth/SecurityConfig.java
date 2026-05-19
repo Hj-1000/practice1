45c82da522b203651d4f2ef27b9d8e17d85e6898
HJ1000 <cjsguswhd1369@gmail.com>
2026-05-19 23:20:02 +0900

    로그인 로직 구현
    2026-05-19

---
+++ src/main/java/com/jeonhong/book/springboot/config/auth/SecurityConfig.java
@@ -0,0 +1,58 @@
+package com.jeonhong.book.springboot.config.auth;
+
+import com.jeonhong.book.springboot.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@Configurable
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                //H2 Console 사용 설정
+                .csrf(csrf -> csrf.disable())
+                .headers(headers ->
+                        headers.frameOptions(frameOption -> frameOption.disable())
+                )
+                // URL 별 권한 관리
+                .authorizeHttpRequests(auth -> auth
+                        // 정적리소스와 메인페이지 누구나 접근 가능
+                        .requestMatchers(
+                                "/",
+                                "/css/**",
+                                "/images/**",
+                                "/js/**",
+                                "/h2-console/**"
+                        ).permitAll()
+
+                        // API는 USER 권한을 가진 사용자만 접근 가능
+                        .requestMatchers("/api/v1/**")
+                        .hasRole(Role.USER.name())
+
+                        // 나머지 요청은 인증된 사용자만 접근 가능
+                        .anyRequest()
+                        .authenticated()
+                )
+                // 로그아웃 성공 시 메인 페이지로 이동
+                .logout(logout ->
+                        logout.logoutSuccessUrl("/")
+                )
+
+                // OAuth2 로그인 설정
+                .oauth2Login(oauth2 ->
+                        oauth2.userInfoEndpoint(userInfo ->
+                                userInfo.userService(customOAuth2UserService)
+                        )
+                );
+
+        return http.build();
+    }
+}
