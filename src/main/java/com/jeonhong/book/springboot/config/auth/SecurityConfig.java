e47ba85e5b9506dcd03adc01a561f945588bf38a
HJ1000 <cjsguswhd1369@gmail.com>
2026-05-20 19:21:18 +0900

    securityConfig 빈등록
    2026-05-20

--- src/main/java/com/jeonhong/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/jeonhong/book/springboot/config/auth/SecurityConfig.java
@@ -2,19 +2,20 @@ package com.jeonhong.book.springboot.config.auth;
 
 import com.jeonhong.book.springboot.domain.user.Role;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.web.SecurityFilterChain;
 
 @RequiredArgsConstructor
-@Configurable
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
 
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 //H2 Console 사용 설정
