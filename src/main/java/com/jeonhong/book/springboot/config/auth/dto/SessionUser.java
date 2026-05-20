b313c39eea77abd3f96c07792ee7a136758f5d2d
HJ1000 <cjsguswhd1369@gmail.com>
2026-05-20 19:31:31 +0900

    세션 직렬화를 위한 코드 추가 2026-05-20

--- src/main/java/com/jeonhong/book/springboot/config/auth/dto/SessionUser.java
+++ src/main/java/com/jeonhong/book/springboot/config/auth/dto/SessionUser.java
@@ -7,6 +7,8 @@ import java.io.Serializable;
 
 @Getter
 public class SessionUser implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private String name;
     private String email;
     private String picture;
