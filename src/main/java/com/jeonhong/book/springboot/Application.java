fdd014358dea21de70039154df908f3cc4d657c9
HJ1000 <cjsguswhd1369@gmail.com>
2026-05-20 22:37:07 +0900

    jpaAuditing 분리 2026-05-20

--- src/main/java/com/jeonhong/book/springboot/Application.java
+++ src/main/java/com/jeonhong/book/springboot/Application.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+//@EnableJpaAuditing를 제거해서 config 쪽으로 옮김
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
